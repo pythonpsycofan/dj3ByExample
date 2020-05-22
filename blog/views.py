20022aacb2b12b66e328bf1ef4ded50740f41488
krzys <pythonpsycofan@gmail.com>
2020-05-22 09:37:57 +0200

    3rd commit - Chapter 1 ready

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
@@ -7,17 +7,25 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.objects.all()
+    paginator = Paginator(object_list, 3)  # 3 post in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
                              slug=post,
-                             status='published',
+                             # status='published',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
-
-
